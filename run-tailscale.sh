ce773d4ea3902561216c3a2a709c8fa771c7b387
Tony <5747393+convexshiba@users.noreply.github.com>
2025-01-26 17:52:45 -0800

    Update run-tailscale.sh
    add machine name

--- run-tailscale.sh
+++ run-tailscale.sh
@@ -3,7 +3,7 @@
 /render/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 PID=$!
 
-until /render/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --advertise-exit-node --hostname="${RENDER_SERVICE_NAME}"; do
+until /render/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --advertise-exit-node --hostname="${TAILSCALE_MACHINE_NAME}"; do
   sleep 0.1
 done
 export ALL_PROXY=socks5://localhost:1055/
